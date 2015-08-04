ddd5ae4458665a2a319a20e4106fec72f330ff69
ianknauer <ian.knauer@gmail.com>
2015-08-03 19:15:05 -0700

    css work

--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,44 @@
 <header class="banner" role="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav role="navigation">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+    <nav class="navbar navbar-black">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="http://davidsuzuki.org">
+            <img alt="Brand" src="<?= get_template_directory_uri() . '/dist/images/
+DSF-Web-White.png'; ?>">
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="">
+         <ul class="nav navbar-nav">
+           <li class="active"><a href="#">ABOUT US</a></li>
+           <li class="dropdown">
+             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ISSUES<span class="caret"></span></a>
+             <ul class="dropdown-menu">
+               <li><a href="#">CLIMATE CHANGE</a></li>
+               <li><a href="#">HEALTH</a></li>
+               <li><a href="#">OCEANS</a></li>
+               <li><a href="#">WILDLIFE & HABITAT</a></li>
+               <li><a href="#">FRESH WATER</a></li>
+             </ul>
+           </li>
+          <li class="active"><a href="#">WHAT YOU CAN DO</a></li>
+          <li class="active"><a href="#">BLOGS</a></li>
+          <li class="active"><a href="#">DAVID</a></li>
+         <li class="dropdown">
+           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DONATE<span class="caret"></span></a>
+           <ul class="dropdown-menu">
+             <li><a href="#">MONTHLY</a></li>
+             <li><a href="#">SINGLE</a></li>
+             <li><a href="#">MORE WAYS TO GIVE</a></li>
+           </ul>
+         </li>
+       </ul>
+       <ul class="nav navbar-nav navbar-secondary">
+         <li class="active"><a href="#">CONTACT</a></li>
+         <li class="active"><a href="#">NEWSLETTER</a></li>
+         <li class="active"><a href="#">FRANÇAIS</a></li>
+       </ul>
+      </div>
     </nav>
   </div>
 </header>
